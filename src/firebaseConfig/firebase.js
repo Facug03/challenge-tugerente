0aebf671f97bc1479307760df5fbd611817e9a41
Facug03 <facundogonzales1550@gmail.com>
2022-12-02 11:53:50 -0300

    configuracion firebase

---
+++ src/firebaseConfig/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDoc,
+  query,
+  where,
+  setDoc,
+  deleteDoc,
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBru8gnCC_BePvZRTtQceGQA_Q1nOesHAE',
+
+  authDomain: 'challenge-tugerente-42694.firebaseapp.com',
+
+  projectId: 'challenge-tugerente-42694',
+
+  storageBucket: 'challenge-tugerente-42694.appspot.com',
+
+  messagingSenderId: '592542399755',
+
+  appId: '1:592542399755:web:497a133828acff80b3b60e',
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
