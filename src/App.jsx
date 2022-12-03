5fde428b84a98ffce680b32ef7c08ec64fd2f0b7
Facug03 <facundogonzales1550@gmail.com>
2022-12-03 11:29:10 -0300

    manejo de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,125 @@
 import { useState, useEffect } from 'react'
-import { getUsers } from './api'
-import AddUser from './components/AddUser'
+import { getUsers } from './services/api'
 
+import AddUser from './components/AddUser'
 import Search from './components/icons/Search'
+import ListUsers from './components/ListUsers'
 
 function App() {
   const [users, setUsers] = useState([])
   const [modal, setModal] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState({
+    totalPages: 0,
+    actualPage: 0,
+    search: '',
+    searchBy: '',
+  })
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    getUsers().then((res) => setUsers(res))
+    getUsers(0)
+      .then((res) => {
+        setUsers(res.data)
+        setPage({ ...page, totalPages: res.totalPages - 1 })
+      })
+      .catch(() => {
+        setError('Un error ha ocurrido')
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
 
+  const onCloseSubmit = () => {
+    setModal(false)
+    setUsers([])
+    setLoading(true)
+    getUsers(0)
+      .then((res) => {
+        setUsers(res.data)
+        setPage({
+          ...page,
+          totalPages: res.totalPages - 1,
+          actualPage: 0,
+          search: '',
+        })
+        setError('')
+      })
+      .catch(() => setError('Un error ha ocurrido'))
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (page.search) {
+      setLoading(true)
+      getUsers(0, page.search, e.target[0].value)
+        .then((res) => {
+          if (res.data.length) {
+            setPage({
+              ...page,
+              actualPage: 0,
+              totalPages: res.totalPages - 1,
+              searchBy: e.target[0].value,
+            })
+            setUsers(res.data)
+            setError('')
+          } else {
+            setUsers([])
+            setError(
+              `No se han encontrado resultados con la palabra ${page.search}`
+            )
+          }
+        })
+        .catch(() => setError('Un error ha ocurrido'))
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+  }
+
+  const nextPage = () => {
+    setLoading(true)
+    setPage({ ...page, actualPage: page.actualPage + 1 })
+    getUsers(page.actualPage + 1, page.search, page.searchBy)
+      .then((res) => {
+        setUsers((bef) => bef.concat(res.data))
+        setError('')
+      })
+      .catch(() => setError('Un error ha ocurrido'))
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
   return (
-    <div className='text-center px-16 font-sans'>
-      <header className='mb-12'>
+    <div className='text-center px-16'>
+      <header className='mb-8'>
         <nav>
-          <h1 className='mt-5'>Usuarios</h1>
-          <form className='flex items-center justify-center h-9'>
-            <select className='w-[115px] h-full rounded-l-2xl bg-red-500 text-white text-base cursor-pointer text-center'>
-              <option>Nombre</option>
-              <option>Razon social</option>
-              <option>Nit</option>
-              <option>Telefono</option>
-              <option>Codigo</option>
+          <h1 className='my-5 text-3xl font-lexendBold text-red-500'>
+            Administración de usuarios
+          </h1>
+          <form
+            onSubmit={handleSubmit}
+            className='flex items-center justify-center h-9'
+          >
+            <select className='w-[120px] font-lexendBold h-full rounded-l-2xl bg-red-500 text-white text-base cursor-pointer text-center'>
+              <option value='nombre'>Nombre</option>
+              <option value='razonSocial'>Razon social</option>
+              <option value='nit'>Nit</option>
+              <option value='telefono'>Telefono</option>
+              <option value='codigo'>Codigo</option>
             </select>
             <input
               className='h-full border-2 border-red-500 outline-none px-1 border-l-0 w-60'
               type='text'
               placeholder='Buscar...'
+              name='search'
+              value={page.search}
+              onChange={(e) => setPage({ ...page, search: e.target.value })}
             />
             <button className='px-3 h-full border-2 border-red-500 border-l-0 rounded-r-2xl'>
               <Search width={20} height={20} />
@@ -36,73 +127,22 @@ function App() {
           </form>
         </nav>
       </header>
-      <main>
-        <div className='bg-slate-700 w-11/12 mx-auto rounded-md text-white'>
-          <div className='flex justify-end py-2 px-4'>
-            <button
-              onClick={() => setModal(true)}
-              className='px-5 py-1 bg-red-500 rounded-2xl'
-            >
-              Anadir
-            </button>
-          </div>
-          <table className='w-full'>
-            <thead className='py-1'>
-              <tr className='border-y border-slate-400 text-left'>
-                <th className='py-2 pl-5'>Nombre</th>
-                <th className='py-2'>Razon social</th>
-                <th className='py-2'>Nit</th>
-                <th className='py-2'>Telefono</th>
-                <th className='py-2'>Codigo</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr className='hover:bg-red-500 ease-in duration-75 text-left'>
-                  <td className='py-3 pl-5'>{user.nombre}</td>
-                  <td className='py-3'>{user.razonSocial}</td>
-                  <td className='py-3'>{user.nit}</td>
-                  <td className='py-3'>{user.telefono}</td>
-                  <td className='py-3'>{user.codigo}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </main>
-      <AddUser modal={modal} onClose={() => setModal(false)} />
+      <ListUsers
+        users={users}
+        changeModal={() => setModal(true)}
+        nextPage={nextPage}
+        loading={loading}
+        page={page}
+        onCloseSubmit={onCloseSubmit}
+        error={error}
+      />
+      <AddUser
+        modal={modal}
+        onCloseSubmit={onCloseSubmit}
+        onClose={() => setModal(false)}
+      />
     </div>
   )
 }
 
 export default App
-
-{
-  /* <tbody>
-<tr className='hover:bg-red-500 ease-in duration-75 text-left'>
-  <td className='py-3 pl-5'>Facundo Gonzalez</td>
-  <td className='py-3'>Facu</td>
-  <td className='py-3'>20-494-24</td>
-  <td className='py-3'>1126622464</td>
-  <td className='py-3'>1267</td>
-</tr>
-</tbody>
-<tbody>
-<tr className='hover:bg-red-500 ease-in duration-75 text-left'>
-  <td className='py-3 pl-5'>Facundo Gonzalez</td>
-  <td className='py-3'>No se</td>
-  <td className='py-3'>20-494-24</td>
-  <td className='py-3'>1126622464</td>
-  <td className='py-3'>1267</td>
-</tr>
-</tbody>
-<tbody>
-<tr className='hover:bg-red-500 ease-in duration-75 text-left'>
-  <td className='py-3 pl-5'>Facundo Gonzalez</td>
-  <td className='py-3'>No se</td>
-  <td className='py-3'>20-494-24</td>
-  <td className='py-3'>1126622464</td>
-  <td className='py-3'>1267</td>
-</tr>
-</tbody> */
-}
