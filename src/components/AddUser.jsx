7e3043649cfdf73d3cc54216c095df90ad7a2aba
Facug03 <facundogonzales1550@gmail.com>
2022-12-03 11:29:23 -0300

    formulario controlado

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,59 +1,147 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
-export default function AddUser({ onClose, modal }) {
+import { createUser } from '../services/api'
+import Loader from './icons/Loader'
+
+export default function AddUser({ onClose, modal, onCloseSubmit }) {
+  const [loading, setLoading] = useState(false)
   const {
     register,
     formState: { errors },
     handleSubmit,
+    reset,
   } = useForm()
+
+  const onSubmit = (data) => {
+    setLoading(true)
+    createUser(data).then(() => {
+      setLoading(false)
+      onCloseSubmit()
+      reset()
+    })
+  }
 
   return (
     <>
       {modal && (
         <div className='w-screen h-screen fixed top-0 left-0 bg-black/50 flex items-center justify-center z-50'>
-          <div className=' bg-white relative border-2 border-black p-5 rounded-md'>
+          <div className=' bg-white relative border-2 border-slate-700 p-5 rounded-md w-80'>
             <button onClick={onClose} className='absolute top-0 right-[6px]'>
               X
             </button>
-            <h2 className='text-2xl mb-2'>Añadir usuario</h2>
-            <form className='flex gap-3 flex-col items-start'>
-              <div>
+            <h2 className='text-2xl mb-2 font-lexendBold text-red-500'>
+              Añadir usuario
+            </h2>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className='flex gap-3 flex-col items-start'
+            >
+              <div className='relative w-full'>
                 <label className='block mb-2'>Nombre</label>
                 <input
-                  className='border-primary px-2 py-1 border-2 rounded-md outline-none'
-                  name='name'
-                />
+                  className='mx-auto border-slate-700 px-2 py-1 border rounded-md outline-none'
+                  type='text'
+                  {...register('nombre', {
+                    required: true,
+                  })}
+                ></input>
+                {errors.nombre?.type === 'required' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Obligatorio
+                  </p>
+                )}
               </div>
-              <div>
-                <label className='block mb-2'>Link</label>
+              <div className='relative w-full'>
+                <label className='block mb-2'>Razón social</label>
                 <input
-                  className='border-primary px-2 py-1 border-2 rounded-md outline-none'
-                  name='link'
-                />
+                  className='mx-auto border-slate-700 px-2 py-1 border rounded-md outline-none'
+                  type='text'
+                  {...register('razonSocial', {
+                    required: true,
+                  })}
+                ></input>
+                {errors.razonSocial?.type === 'required' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Obligatorio
+                  </p>
+                )}
               </div>
-              <div>
-                <label className='block mb-2'>Nombre</label>
+              <div className='relative w-full'>
+                <label className='block mb-2'>Nit</label>
                 <input
-                  className='border-primary px-2 py-1 border-2 rounded-md outline-none'
-                  name='name'
-                />
+                  className='mx-auto border-slate-700 px-2 py-1 border rounded-md outline-none'
+                  type='text'
+                  {...register('nit', {
+                    required: true,
+                  })}
+                ></input>
+                {errors.nit?.type === 'required' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Obligatorio
+                  </p>
+                )}
               </div>
-              <div>
-                <label className='block mb-2'>Link</label>
+              <div className='relative w-full'>
+                <label className='block mb-2'>Teléfono</label>
                 <input
-                  className='border-primary px-2 py-1 border-2 rounded-md outline-none'
-                  name='link'
-                />
+                  className='mx-auto border-slate-700 px-2 py-1 border rounded-md outline-none'
+                  type='text'
+                  {...register('telefono', {
+                    required: true,
+                    pattern: /^[0-9]*$/,
+                    maxLength: 10,
+                  })}
+                ></input>
+                {errors.telefono?.type === 'required' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Obligatorio
+                  </p>
+                )}
+                {errors.telefono?.type === 'pattern' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Solo números
+                  </p>
+                )}
+                {errors.telefono?.type === 'maxLength' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Maximo 10 números
+                  </p>
+                )}
               </div>
-              <div>
-                <label className='block mb-2'>Nombre</label>
+              <div className='relative w-full'>
+                <label className='block mb-2'>Código</label>
                 <input
-                  className='border-primary px-2 py-1 border-2 rounded-md outline-none'
-                  name='name'
-                />
+                  className='mx-auto border-slate-700 px-2 py-1 border rounded-md outline-none'
+                  type='text'
+                  {...register('codigo', {
+                    required: true,
+                    pattern: /^[0-9]*$/,
+                    maxLength: 4,
+                  })}
+                ></input>
+                {errors.codigo?.type === 'required' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Obligatorio
+                  </p>
+                )}
+                {errors.codigo?.type === 'pattern' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Solo números
+                  </p>
+                )}
+                {errors.codigo?.type === 'maxLength' && (
+                  <p className='absolute left-[23px] text-xs text-red-700'>
+                    Maximo 4 números
+                  </p>
+                )}
               </div>
-              <button className='bg-primary py-2 px-4 text-white rounded-md hover:bg-secondary duration-200 ease-linear'>
-                Añadir
+              <button className='mx-auto mt-2 bg-red-500 py-2 w-[83px] text-white rounded-md hover:bg-red-400 duration-200 ease-linear'>
+                {loading ? (
+                  <Loader h='h-6' w='w-6' color='fill-white' />
+                ) : (
+                  'Añadir'
+                )}
               </button>
             </form>
           </div>
@@ -63,29 +151,30 @@ export default function AddUser({ onClose, modal }) {
   )
 }
 
-// <div className={style.container_input}>
-// <input
-//   required
-//   className={style.input}
-//   // placeholder="Email"
-//   type="text"
-//   {...register("email", {
-//     required: true,
-//     pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-//   })}
-// ></input>
-// <span></span>
-// <label className={style.labelText}>Correo</label>
-// {errors.email?.type === "required" && (
-//   <p className={style.error}>Obligatorio</p>
-// )}
-// {errors.email?.type === "pattern" && (
-//   <div className={style.error}>
-//     <p>Ingresa un correo válido</p>
-//     <i class="fa-solid fa-circle-exclamation"></i>
-//   </div>
-// )}
-// </div>
+{
+  /* <div className={style.container_input}>
+<input
+  required
+  className={styl
+  type="text"
+  {...register("email", {
+    required: true,
+    pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+  })}
+></input>
+<span></span>
+<label className={style.labelText}>Correo</label>
+{errors.email?.type === "required" && (
+  <p className={style.error}>Obligatorio</p>
+)}
+{errors.email?.type === "pattern" && (
+  <div className={style.error}>
+    <p>Ingresa un correo válido</p>
+    <i class="fa-solid fa-circle-exclamation"></i>
+  </div>
+)}
+</div> */
+}
 // <div className={style.container_input}>
 // <input
 //   required
